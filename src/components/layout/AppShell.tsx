b18156173b4a34ee7c9014037ad9ec222c6a1a29
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-05-18 09:27:41 +0100

    changed logo

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -23,12 +23,12 @@ export function AppShell({ children }: Props) {
 
       {/* FULL WIDTH FOOTER */}
       <footer className=" border-t border-white bg-surface900 flex flex-col gap-4 sm:flex-row sm:items-center justify-between px-6 py-2.5">
-        <div className="flex items-center gap-2 text-[10px] sm:text-xs text-white font-lexend">
+        <div className="flex items-center justify-between gap-2 text-[10px] sm:text-xs text-white font-lexend">
           Designed and developed by Events Intelligence & Technology Services
           Limited.
           <img src="/events-intel.png" alt="" className="h-10" />
         </div>
-        <div className="text-[10px] sm:text-xs text-white font-lexend flex items-center gap-1.5">
+        <div className="text-[10px] sm:text-xs text-white font-lexend flex items-center justify-center gap-1.5">
           AI Deployment Partner
           <img src="/rise-networks-logo.png" alt="" className="h-10"/>
         </div>
