47fd4d579cdec29131955174f92d0c6af49d3cea
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-05-26 14:32:33 +0100

    fixed ui

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -14,56 +14,70 @@ function useLiveClock() {
 export function TopNav() {
   const now = useLiveClock();
 
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
 
-  function handleOpenMenu(){
-    setShowMenu(true)
+  function handleOpenMenu() {
+    setShowMenu(true);
   }
 
-  function handleCloseMenu(){
-    setShowMenu(false)
+  function handleCloseMenu() {
+    setShowMenu(false);
   }
 
   return (
     <>
-    <MobileSidebar handleCloseMenu={handleCloseMenu} showMenu={showMenu}/>
-    <header className="lg:h-20 bg-surface900 border-b font-lexend border-white/45 py-5 lg:py-0 lg:divide-x divide-white gap-4 lg:gap-0 px-4  lg:px-6 flex flex-col lg:flex-row lg:items-center">
-      {/* Logo + brand */}
-      <img src="./logo_il.png" alt=""className="md:w-25 md:h-15 px-2 w-20 h-15" />
-      <div className="flex justify-between items-center gap-3 lg:basis-2/8 lg:px-2">
-        <img src="/invest-lagos.png" alt="" className="w-30 lg:w-40"/>
-        <button onClick={handleOpenMenu} className="cursor-pointer lg:hidden"><Menu className="w-6 h-6 text-white"/></button>
-      </div>
+      <MobileSidebar handleCloseMenu={handleCloseMenu} showMenu={showMenu} />
+      <header className="lg:h-20 bg-surface900 border-b font-lexend border-white/45 py-5 lg:py-0 lg:divide-x divide-white gap-4 lg:gap-0 px-4  lg:px-6 flex flex-col lg:flex-row lg:items-center">
+        {/* Logo + brand */}
+        <div className="lg:divide-x divide-white flex items-center justify-between">
+          <div className="flex-1">
+            <img
+            src="./logo_il.png"
+            alt=""
+            className="md:w-25 md:h-15 px-2 w-20 h-15"
+          />
+          </div>
+          
+          <div className="flex justify-between items-center gap-3 flex-1 lg:basis-2/8 lg:px-2">
+            <img src="/invest-lagos.png" alt="" className="w-30 lg:w-40" />
+            <button
+              onClick={handleOpenMenu}
+              className="cursor-pointer lg:hidden"
+            >
+              <Menu className="w-6 h-6 text-white" />
+            </button>
+          </div>
+        </div>
 
-      {/* Current session */}
-      <div className="lg:basis-3/8 lg:px-4">
-        <div className="text-[10px] sm:text-xs font-light tracking-widest text-white uppercase">
-          Current Session
+        {/* Current session */}
+        <div className="lg:basis-3/8 lg:px-4">
+          <div className="text-[10px] sm:text-xs font-light tracking-widest text-white uppercase">
+            Current Session
+          </div>
+          <div className="text-lg sm:text-xl text-cyan font-semibold mt-0.75 truncate">
+            Lagos &mdash; Africa's Global Gateway
+          </div>
+          <div className="text-[10px] sm:text-xs text-white font-light mt-0.75">
+            8th to 9th June 2026 | Eko Convention Center, NIGERIA
+          </div>
         </div>
-        <div className="text-lg sm:text-xl text-cyan font-semibold mt-0.75 truncate">
-          Lagos &mdash; Africa's Global Gateway
-        </div>
-        <div className="text-[10px] sm:text-xs text-white font-light mt-0.75">
-          8th to 9th June 2026 | Eko Convention Center, NIGERIA
-        </div>
-      </div>
 
-      {/* Live clock */}
-      <div className="lg:basis-3/8 lg:px-4">
-        <div className="flex justify-between gap-4 items-center">
-          <div>
-            <div className="text-[10px] sm:text-xs font-light tracking-widest text-white uppercase">
-              Live Clock
+        {/* Live clock */}
+        <div className="lg:basis-3/8 lg:px-4">
+          <div className="flex justify-between gap-4 items-center">
+            <div>
+              <div className="text-[10px] sm:text-xs font-light tracking-widest text-white uppercase">
+                Live Clock
+              </div>
+              <div className="text-lg sm:text-2xl font-semibold text-green mt-1 tabular-nums">
+                {now.toLocaleTimeString("en-GB", { hour12: false })}
+              </div>
             </div>
-            <div className="text-lg sm:text-2xl font-semibold text-green mt-1 tabular-nums">
-              {now.toLocaleTimeString("en-GB", { hour12: false })}
-            </div>
+
+            <img src="/commonwealth.png" alt="" className="w-30 lg:w-40" />
           </div>
 
-          <img src="/commonwealth.png" alt="" className="w-30 lg:w-40"/>
-        </div>
-
-        {/* <div className="text-xs text-slate-500 mt-1">
+          {/* <div className="text-xs text-slate-500 mt-1">
             {now.toLocaleDateString('en-GB', {
               weekday: 'short',
               day: 'numeric',
@@ -71,11 +85,11 @@ export function TopNav() {
               year: 'numeric',
             })}
           </div> */}
-        <div className="text-[10px] sm:text-xs text-white font-light mt-1">
-          8th to 9th June 2026 | Eko Convention Center, NIGERIA
+          <div className="text-[10px] sm:text-xs text-white font-light mt-1">
+            8th to 9th June 2026 | Eko Convention Center, NIGERIA
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
     </>
   );
 }
